1e4e48537c72eb06170600de2b4bcdd90ee36fe1
KevinPham <kevinpham15@gmail.com>
2017-11-08 19:55:34 -0800

    updated with add/update functionality

--- BucketList/BucketList/AddItemTableViewController.swift
+++ BucketList/BucketList/AddItemTableViewController.swift
@@ -15,6 +15,7 @@ import UIKit
 
 class AddItemTableViewController: UITableViewController{
 
+    
     weak var delegate: AddItemTableViewControllerDelegate?
     
     var item: String?
--- BucketList/BucketList/BucketListViewController.swift
+++ BucketList/BucketList/BucketListViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import CoreData
+
 
 //==============================================================//
 //========================= Main page view =====================//
@@ -17,6 +19,8 @@ class BucketListViewController: UITableViewController, AddItemTableViewControlle
     // variable created = AddItemButton
     @IBOutlet weak var AddItemButton: UIBarButtonItem!
     
+    
+    // performSegue ==============================================
     // function for the actual button being pressed
     @IBAction func AddItemPressed(_ sender: UIBarButtonItem) {
         performSegue(withIdentifier: "itemSegue", sender: AddItemButton)
@@ -24,13 +28,14 @@ class BucketListViewController: UITableViewController, AddItemTableViewControlle
         
     }
     
+    var items = [BucketListItem]()
     
+    let managedObjectContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
-    var items = [String]()
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchAllItems()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -39,9 +44,6 @@ class BucketListViewController: UITableViewController, AddItemTableViewControlle
         // Dispose of any resources that can be recreated.
     }
 
-    
-    
-    
     
     // this func is "override" bc we are inheriting from UITableViewController
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -52,7 +54,7 @@ class BucketListViewController: UITableViewController, AddItemTableViewControlle
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell", for: indexPath)
-        cell.textLabel?.text = items[indexPath.row]
+        cell.textLabel?.text = items[indexPath.row].text!
         return cell
     }
     
@@ -78,28 +80,41 @@ class BucketListViewController: UITableViewController, AddItemTableViewControlle
     // just having this func, gives functionality for  > swipe left for delete button
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         
+        let item = items[indexPath.row]
+        managedObjectContext.delete(item)
+        do {
+            try managedObjectContext.save()
+        } catch {
+            print("\(error)")
+        }
         items.remove(at: indexPath.row)
-        
         tableView.reloadData()
     }
     
-    // segue prepare which overrides "performSegue" function
+    
+    
+    //PREPARE SEGUE =============================================================
+
         // for segue: specifically looking for segues in storyboard
         //sender: what object type sent the request(i.e. bar button item or button, any object,....)
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         
         //storyboard path way
+        // *identifier* is important so that you can access different segues.
         if segue.identifier == "itemSegue" {
+            
+            // segue'd from ADD button
             let navigationController = segue.destination as! UINavigationController
             let AddItemTableViewController = navigationController.topViewController as! AddItemTableViewController
             AddItemTableViewController.delegate = self
             
-            
+            // segue'd from EDIT button
             if let indexPath = sender as? NSIndexPath{
                 let item = items[indexPath.row]
                 
-                AddItemTableViewController.item = item
+                AddItemTableViewController.item = item.text!                         // add .text!, previously a bug
                 AddItemTableViewController.indexPath = indexPath
                 // the index [] in the array of "items".
             }
@@ -107,23 +122,66 @@ class BucketListViewController: UITableViewController, AddItemTableViewControlle
     }
     
     
+    //CORE DATA ============================================================= CORE DATA//
+    
+    func fetchAllItems() {
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "BucketListItem")
+        do {
+            let result = try managedObjectContext.fetch(request)
+            items = result as! [BucketListItem]
+        } catch {
+            print("\(error)")
+        }            
+    }
+    
+    //CORE DATA ============================================================= CORE DATA//
+    
+    
+    
+    
+    
+    
+    
+    
+    // functions below should go into Extension, which exists outside of the class ==========****===========
+    
     func CancelButtonPressed(by controller: AddItemTableViewController) {
         print ("I'm the hidden controller, BUT I am responding to the CANCEL button press on the top view controller.")
         dismiss(animated: true, completion: nil)
         // dismiss the top view controller, because it cannot do that action itself.
     }
     
+    
+    
+    
     func itemSaved(by controller: AddItemTableViewController, with text: String, at indexPath: NSIndexPath?) {
         print ("Received Text From Top View: \(text)")
         
         if let ip = indexPath{
-            items[ip.row] = text
-            //updates the old entry "overwrite existing"
+            let item = items[ip.row]
+            //updates the old entry "overwrite existing
+            
+            item.text = text
+            // text is an atribute from BucketList data file
+            
+            
             
         } else {
-            items.append(text)
+            let item = NSEntityDescription.insertNewObject(forEntityName: "BucketListItem", into: managedObjectContext) as! BucketListItem
+            item.text = text
+            // text is an atribute from BucketList data file
+            
+            items.append(item)
             //creates a new entry at the end of the items array
         }
+        
+        do {
+            try managedObjectContext.save()
+        } catch {
+            print("\(error)")
+        }
+        
   
         tableView.reloadData()
             // this reloads the page so the new data is added and seen
